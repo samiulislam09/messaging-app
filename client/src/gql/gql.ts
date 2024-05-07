132101122dd3609de620005fcbd32be235c0b77b
samiulislam09 <samiulislamsawon09@gmail.com>
2024-05-07 18:03:53 +0600

    added file upload

--- client/src/gql/gql.ts
+++ client/src/gql/gql.ts
@@ -14,6 +14,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  */
 const documents = {
     "\nmutation CreateProfile($input: CreateProfileDto!) {\n    createProfile(input: $input) {\n        id\n        imageUrl\n        name\n        email\n    }\n}\n": types.CreateProfileDocument,
+    "\n    mutation CreateServer($input: CreateServerDto!, $file: Upload) {\n        createServer(input: $input, file: $file) {\n            id\n            name\n            imgUrl\n            members {\n                id\n            }\n            \n        }\n    }\n\n": types.CreateServerDocument,
 };
 
 /**
@@ -34,6 +35,10 @@ export function graphql(source: string): unknown;
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\nmutation CreateProfile($input: CreateProfileDto!) {\n    createProfile(input: $input) {\n        id\n        imageUrl\n        name\n        email\n    }\n}\n"): (typeof documents)["\nmutation CreateProfile($input: CreateProfileDto!) {\n    createProfile(input: $input) {\n        id\n        imageUrl\n        name\n        email\n    }\n}\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n    mutation CreateServer($input: CreateServerDto!, $file: Upload) {\n        createServer(input: $input, file: $file) {\n            id\n            name\n            imgUrl\n            members {\n                id\n            }\n            \n        }\n    }\n\n"): (typeof documents)["\n    mutation CreateServer($input: CreateServerDto!, $file: Upload) {\n        createServer(input: $input, file: $file) {\n            id\n            name\n            imgUrl\n            members {\n                id\n            }\n            \n        }\n    }\n\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
